61fc4b1de65cccb1f5c9288dcaf734112142afac
Michael Brooke <michaelbrooke93@gmail.com>
2024-10-22 21:39:08 +0200

    Create Enemy and Player projectiles that move vertically and do damage on collisions

---
+++ Assets/Projectile/ProjectileMovement.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Moves a projectile across the screen vertically at the configured speed
+/// </summary>
+public class ProjectileMovement : MonoBehaviour
+{
+    private enum Direction
+    {
+        Up,
+        Down
+    }
+
+    [Tooltip("Speed at which the projectile travels")]
+    [SerializeField] private float movementSpeed = 10f;
+
+    [Tooltip("Direction the projectile moves")]
+    [SerializeField] private Direction direction;
+
+    private void Update() => transform.position += TransformDirection() * (movementSpeed * Time.deltaTime);
+
+    private Vector3 TransformDirection() =>
+        direction switch
+        {
+            Direction.Up => transform.up,
+            Direction.Down => transform.up * -1,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+}
